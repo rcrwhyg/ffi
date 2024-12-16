0cab298f363d345861cdcbc6fc1f02bc0a8e06da
rcrwhyg <rcrwhyg@sina.com>
2024-12-16 14:49:54 +0800

    feature: support pyo3

---
+++ python-binding/src/lib.rs
@@ -0,0 +1,18 @@
+use hasher::PyAlgo;
+use pyo3::prelude::*;
+
+mod hasher;
+
+/// Prints a message.
+#[pyfunction]
+fn hello() -> PyResult<String> {
+    Ok("Hello from python-binding!".into())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn _lowlevel(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(hello, m)?)?;
+    m.add_class::<PyAlgo>()?;
+    Ok(())
+}
